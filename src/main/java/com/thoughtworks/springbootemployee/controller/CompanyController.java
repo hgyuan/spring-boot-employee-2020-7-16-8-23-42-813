eb33a9b1f5425abad16f6969ad91ff2cea0bbdcd
1764775976@qq.com <1764775976@qq.com>
2020-07-28 20:46:36 +0800

    [springboot employee][richard,jabin] feature: add CompanyController

---
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -0,0 +1,54 @@
+package com.thoughtworks.springbootemployee.controller;
+
+import com.thoughtworks.springbootemployee.entity.Company;
+import com.thoughtworks.springbootemployee.entity.Employee;
+import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/companies")
+public class CompanyController {
+
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @PostMapping()
+    public void addCompany(@RequestBody Company company) {
+        companyRepository.addCompany(company);
+    }
+
+    @PutMapping()
+    public void updateCompany(@RequestBody Company company) {
+        companyRepository.updateCompany(company);
+    }
+
+    @DeleteMapping()
+    public void deleteEmployeesOfCompany(Integer companyId) {
+        companyRepository.deleteEmployeesOfCompany(companyId);
+    }
+
+    @GetMapping()
+    public List<Company> getCompanies(@RequestParam(value = "page", required = false) Integer page,
+                                      @RequestParam(value = "pageSize", required = false) Integer pageSize) {
+        if (page != null && pageSize != null) {
+            return companyRepository.queryCompanyByPage(page,pageSize);
+        }
+        return companyRepository.getCompanies();
+    }
+
+    @GetMapping("/{companyId}")
+    public Company getEmployees(@RequestParam("companyId") Integer companyId) {
+        return companyRepository.getCompany(companyId);
+    }
+
+    @GetMapping("/{companyId}/employees")
+    public List<Employee> getEmployeesByCompanyId(@PathVariable("companyId") Integer companyId) {
+        return companyRepository.getCompany(companyId).getEmployees();
+    }
+
+
+}
