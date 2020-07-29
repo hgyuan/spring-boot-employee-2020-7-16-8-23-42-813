58ceb7e168039d1c78c77c8106df9a0ba26acce6
1764775976@qq.com <1764775976@qq.com>
2020-07-29 18:25:04 +0800

    [springboot employee][richard,jabin] feacture： add entity of Employee and Company

---
+++ src/main/java/com/thoughtworks/springbootemployee/entity/Company.java
@@ -0,0 +1,43 @@
+package com.thoughtworks.springbootemployee.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Table(name = "company")
+@Entity
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    @OneToMany(mappedBy = "company")
+    @JsonIgnore
+    private List<Employee> employees;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
---
+++ src/main/java/com/thoughtworks/springbootemployee/entity/Employee.java
@@ -0,0 +1,67 @@
+package com.thoughtworks.springbootemployee.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String gender;
+    @JoinColumn(name = "company_id")
+    @JsonIgnore
+    @ManyToOne
+    private Company company;
+
+
+    public Employee() {
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+
+}
