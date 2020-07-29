27a0efe53cbf04cebb5522a19c2035dcf8982aea
1764775976@qq.com <1764775976@qq.com>
2020-07-29 21:45:38 +0800

    [springboot employee][richard,jabin] feature: add EmployeeRequestDto

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -1,7 +1,12 @@
 package com.thoughtworks.springbootemployee.controller;
 
+import com.fasterxml.jackson.databind.util.BeanUtil;
+import com.thoughtworks.springbootemployee.dto.EmployeeRequestDto;
+import com.thoughtworks.springbootemployee.entity.Company;
 import com.thoughtworks.springbootemployee.entity.Employee;
+import com.thoughtworks.springbootemployee.service.CompanyService;
 import com.thoughtworks.springbootemployee.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
@@ -15,13 +20,18 @@ import java.util.Objects;
 public class EmployeeController {
 
     private final EmployeeService employeeService;
+    private final CompanyService companyService;
 
-    public EmployeeController(EmployeeService employeeService) {
+    public EmployeeController(EmployeeService employeeService, CompanyService companyService) {
         this.employeeService = employeeService;
+        this.companyService = companyService;
     }
 
     @PostMapping()
-    public void addEmployee(@RequestBody Employee employee) {
+    public void addEmployee(@RequestBody EmployeeRequestDto employeeRequestDto) {
+        Employee employee = new Employee();
+        BeanUtils.copyProperties(employeeRequestDto,employee);
+        employee.setCompany(companyService.queryCompanyById(employeeRequestDto.getCompanyId()));
         employeeService.addEmployee(employee);
     }
 
@@ -51,7 +61,7 @@ public class EmployeeController {
     }
 
     @GetMapping("/{employeeId}")
-    public Employee findEmployeesById(@PathVariable Integer employeeId) {
+    public Employee queryEmployeesById(@PathVariable Integer employeeId) {
         return employeeService.queryEmployeeById(employeeId);
     }
 }
---
+++ src/main/java/com/thoughtworks/springbootemployee/dto/EmployeeResponseDto.java
@@ -0,0 +1,52 @@
+package com.thoughtworks.springbootemployee.dto;
+
+public class EmployeeResponseDto {
+
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String gender;
+    private Integer companyId;
+
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Integer companyId) {
+        this.companyId = companyId;
+    }
+}
