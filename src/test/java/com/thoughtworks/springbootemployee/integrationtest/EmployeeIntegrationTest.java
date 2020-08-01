b92a537399a575b91247f95e9c1f4f8c9ecbd9f6
1764775976@qq.com <1764775976@qq.com>
2020-08-01 20:35:39 +0800

    [springboot employee][richard,olivia] test:should_not_contain_female_when_find_employee_by_gender_given_male

--- src/test/java/com/thoughtworks/springbootemployee/integrationtest/EmployeeIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/integrationtest/EmployeeIntegrationTest.java
@@ -15,11 +15,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -48,7 +46,7 @@ public class EmployeeIntegrationTest {
         Company company = new Company();
         company.setName("ri");
         companyRepository.save(company);
-        Employee employee = new Employee("ri",12,"male",company);
+        Employee employee = new Employee("ri", 12, "male", company);
         employeeRepository.save(employee);
     }
 
@@ -59,7 +57,7 @@ public class EmployeeIntegrationTest {
                 "\t\"name\":\"ri\",\n" +
                 "\t\"age\": 11,\n" +
                 "\t\"gender\":\"male\",\n" +
-                "\t\"companyId\":"+company.getId()+"\n" +
+                "\t\"companyId\":" + company.getId() + "\n" +
                 "}";
         mockMvc.perform(post("/employees")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -72,37 +70,37 @@ public class EmployeeIntegrationTest {
         Company company = companyRepository.findAll().stream().findFirst().get();
         Employee employee = employeeRepository.findAll().stream().findFirst().get();
         String employeeDto = "{\n" +
-                "        \"id\": "+
-                employee.getId()+
+                "        \"id\": " +
+                employee.getId() +
                 "         ,\n" +
                 "        \"name\": \"chengcheng111\",\n" +
                 "        \"age\": 54,\n" +
                 "        \"gender\": \"male\",\n" +
-                "        \"companyId\":"+
-                company.getId()+
-                         "\n" +
+                "        \"companyId\":" +
+                company.getId() +
+                "\n" +
                 " }";
 
-        mockMvc.perform(put("/employees/"+employee.getId()).contentType(MediaType.APPLICATION_JSON).content(employeeDto))
+        mockMvc.perform(put("/employees/" + employee.getId()).contentType(MediaType.APPLICATION_JSON).content(employeeDto))
                 .andExpect(status().isAccepted());
     }
 
     @Test
     void should_return_status_200_when_delete_employee_given_employee_employee_id() throws Exception {
         Employee employee = employeeRepository.findAll().stream().findFirst().get();
-        mockMvc.perform(delete("/employees/"+employee.getId()))
+        mockMvc.perform(delete("/employees/" + employee.getId()))
                 .andExpect(status().isOk());
-        assertEquals(Optional.empty(),employeeRepository.findById(employee.getId()));
+        assertEquals(Optional.empty(), employeeRepository.findById(employee.getId()));
     }
 
     @Test
     void should_return_status_200_when_employees_given_4_employee_unpage_false() throws Exception {
         tearDown();
-        for(int i=0;i<4;i++){
+        for (int i = 0; i < 4; i++) {
             Company company = new Company();
             company.setName("ri");
             companyRepository.save(company);
-            Employee employee = new Employee(String.valueOf(i),12,"male",company);
+            Employee employee = new Employee(String.valueOf(i), 12, "male", company);
             employeeRepository.save(employee);
         }
         mockMvc.perform(get("/employees?unpaged=false"))
@@ -110,5 +108,16 @@ public class EmployeeIntegrationTest {
                 .andExpect(jsonPath("[0].name").value(2));
     }
 
-
+    @Test
+    void should_not_contain_female_when_find_employee_by_gender_given_male() throws Exception {
+        String result = mockMvc.perform(get("/employees?gender=male"))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        boolean isContain = result.contains("female");
+        assertFalse(isContain);
+        isContain = result.contains("male");
+        assertTrue(isContain);
+    }
 }
