3b988001a90214ce838525e313469c865e647447
olivia-wang-code <2350625418@qq.com>
2020-08-01 23:33:03 +0800

    [springboot employee][richard,olivia]feature: pass should_return_status_200_when_delete_company_given_company

--- src/test/java/com/thoughtworks/springbootemployee/integrationtest/CompanyIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/integrationtest/CompanyIntegrationTest.java
@@ -1,8 +1,10 @@
 package com.thoughtworks.springbootemployee.integrationtest;
 
 import com.thoughtworks.springbootemployee.entity.Company;
+import com.thoughtworks.springbootemployee.entity.Employee;
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,10 +13,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -30,6 +32,13 @@ public class CompanyIntegrationTest {
     @AfterEach
     void tearDown() {
         companyRepository.deleteAll();
+    }
+
+    @BeforeEach
+    void initEntity() {
+        Company company = new Company();
+        company.setName("ri");
+        companyRepository.save(company);
     }
 
     @Test
@@ -70,5 +79,12 @@ public class CompanyIntegrationTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(company))
                 .andExpect(status().is2xxSuccessful());
+    }
+    @Test
+    void should_return_status_200_when_delete_company_given_company() throws Exception {
+        Company company = companyRepository.findAll().stream().findFirst().get();
+        mockMvc.perform(delete("/companies/" + company.getId()))
+                .andExpect(status().isOk());
+        assertEquals(Optional.empty(), companyRepository.findById(company.getId()));
     }
 }
